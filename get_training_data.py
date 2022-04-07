bc40ec87ad64dbbfc05faee257a10c6fde598a8c
João Gouveia <j.lucas.gouveia@gmail.com>
2022-04-07 03:48:37 +0100

    improved the get trainning data script

--- get_training_data.py
+++ get_training_data.py
@@ -3,6 +3,8 @@ import time
 import argparse
 import os
 from datetime import datetime
+import mediapipe as mp
+import numpy as np
 
 
 # Get the command line args
@@ -23,23 +25,51 @@ if not os.path.isdir(dir):
 # Set Capture Device
 cap = cv2.VideoCapture(0) 
 
-# Warmup...
-time.sleep(2)
-
 path, dirs, files = next(os.walk(dir))
 
 frame_num = len(files)
 
-print(frame_num)
+mp_selfie_segmentation = mp.solutions.selfie_segmentation
+selfie_segmentation = mp_selfie_segmentation.SelfieSegmentation(model_selection=1)
+
+# Warmup...
+time.sleep(2)
 
 start_time = datetime.now()
 print("Capturing frames, started at " + start_time.strftime("%H:%M:%S"), "and will be done until reaches 10000 frames")
 while(frame_num < 10000):
 	# Capture frame-by-frame
 	ret, frame = cap.read()
-	cv2.imwrite(os.path.join(dir, str(frame_num) + ".jpg"), frame)
+	cv2.imshow("getting images for " + args.classification + " - Press ESC to quit", frame)
+	# flip the frame to horizontal direction
+	image = cv2.flip(frame, 1)
+	height , width, channel = image.shape
+
+	RGB = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+	# get the result
+	results = selfie_segmentation.process(RGB)
+
+	# extract segmented mask
+	mask = results.segmentation_mask
+	condition = np.stack(
+		(results.segmentation_mask,) * 3, axis=-1) > 0.5
+
+		# resize the background image to the same size of the original frame
+	bg_image = np.zeros((height,width,3), np.uint8)
+	bg_image = cv2.resize(bg_image, (width, height))
+
+	output_image = np.where(condition, image, bg_image)
+
+	cv2.imwrite(os.path.join(dir, str(frame_num) + ".jpg"), output_image)
 	frame_num += 1
 	time.sleep(0.6)
+	k = cv2.waitKey(30) & 0xff
+	if k == 27:
+		break
+
+
+#closing all open windows 
+cv2.destroyAllWindows() 
 
 # close the already opened camera
 cap.release()
